ba8a883f6f1d78d1e1fac894774a8478463de699
koassumo <0770407@gmail.com>
2020-06-01 02:19:27 +0300

    ДЗ 4 создан пакет DATA, интерфейс IOpenWeather, прописаны зависимости

---
+++ app/src/main/java/ru/geekbrains/android2/semenovweather/ui/home/IOpenWeather.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.android2.semenovweather.ui.home;
+
+import com.geekbrains.a1l8_openweathermap.rest.entities.WeatherRequestRestModel;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface IOpenWeather {
+    @GET("data/2.5/weather")
+    Call<WeatherRequestRestModel> loadWeather(@Query("q") String city,
+                                              @Query("appid") String keyApi,
+                                              @Query("units") String units);
+}
---
+++ app/src/main/java/ru/geekbrains/android2/semenovweather/ui/home/data/WindRestModel.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.android2.semenovweather.ui.home.data;
+
+import com.google.gson.annotations.SerializedName;
+
+public class WindRestModel {
+    @SerializedName("speed") public float speed;
+    @SerializedName("deg") public float deg;
+}
